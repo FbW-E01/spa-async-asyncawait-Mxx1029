c48640cf4b8ea36c3e2aa671ecc75d37ff13ffaa
Mxx1029 <lia.pim@icloud.com>
2021-06-28 16:18:50 +0200

    Initial import

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,16 +2,31 @@ import "../styles/main.scss";
 import "babel-polyfill"
 
 const modal = document.getElementById("myModal");
-let promiseOfModal = new Promise(function (resolve) {
-    window.setTimeout(function () {
-        resolve(modal)
-    }, (1000 * 60));
-});
+// let promiseOfModal = new Promise(function (resolve) {
+//     window.setTimeout(function () {
+//         resolve(modal)
+//     }, (1000 * 2));
+// });
 
-promiseOfModal.then(function(val) {
+// i don't understand how to start looking for what i need to do 
+let promiseOfModal = async function () {
+    window.setTimeout(() => {
+        return modal;
+    }, (1000 * 5));
+};
+
+try {
+    promiseOfModal();
     console.log("User has been on the page for 60 seconds");
-    val.style.display = "block";
-})
+    modal.style.display = "block";
+} catch (e) {
+    console.log("error", e);
+}
+
+// promiseOfModal.then(function(val) {
+//     console.log("User has been on the page for 60 seconds");
+//     val.style.display = "block";
+// })
 
 modal.addEventListener("click", (e) => {
     switch(e.target.className) {
@@ -21,4 +36,12 @@ modal.addEventListener("click", (e) => {
         break;
     }
 })
+
+const button = document.querySelector("#continue");
+
+button.addEventListener('animationend', () => {
+    button.style.backgroundColor = "magenta";
+    alert("Continue to subscribe")
+    });
+    
 
